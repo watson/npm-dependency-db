3bcb67b08f354027ac785e8d81af8a10d27dff5c
Thomas Watson Steen <w@tson.dk>
2016-12-13 19:28:38 +0100

    updater: default live property to false

--- updater.js
+++ updater.js
@@ -26,7 +26,7 @@ function Updater (db, opts) {
   this._depDb = new DepDb(this._lvlDb)
 
   this.key = opts.key || '503cf9e12d8ae49a07568d90f04bbdbfa3f1998ab97ed2b5143c1f3f69ec052f'
-  this.live = opts.live
+  this.live = opts.live || false
   this.feed = hypercore(this._lvlDb).createFeed(this.key)
 
   this.startBlock = 0
